67df7310f745e8aef43c544576483483ff77a3af
C_Jyo <minalikong@gmail.com>
2022-11-28 19:37:36 +0900

    index.js의 사용하지 않는 기능 삭제

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,4 @@ const commentRouter = require("./comments")
 router.use("/posts", [postRouter]);
 router.use("/comments", commentRouter);
 
-router.get("/", (req, res) =>{
-    res.json({message: "OK"})
-})
-
 module.exports = router;
