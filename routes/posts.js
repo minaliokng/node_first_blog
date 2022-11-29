7af07c1e7ab6c4455dd6b2173fb8a6b409ce10c1
C_Jyo <minalikong@gmail.com>
2022-11-29 09:41:56 +0900

    한국 시간대로 표기 변경

--- routes/posts.js
+++ routes/posts.js
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
   const month = ('0' + (today.getMonth() + 1)).slice(-2);
   const day = ('0' + today.getDate()).slice(-2);
 
-  const hours = ('0' + today.getHours()).slice(-2); 
+  const hours = (Number(('0' + today.getHours()).slice(-2))+9) % 24;
   const minutes = ('0' + today.getMinutes()).slice(-2);
   const seconds = ('0' + today.getSeconds()).slice(-2);
   const date = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
